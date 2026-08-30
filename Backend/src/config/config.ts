cde24800672c8f418753eb2a9b7e1d2c1a04dabb
Aditya218-maker <aditya777anand@gmail.com>
2026-08-30 10:41:41 +0530

    feat: define CONFIG type for API keys in config.ts

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -1,2 +1,12 @@
 import { config } from "dotenv";
 config();
+/**
+ * We will define API keys of our three AI agents here which are basically strings
+ */
+
+type CONFIG = {
+    GOOGLE_API_KEY: string;
+    MISTRAL_APIA_KEY: string;
+    COHERE_API_KEY: string;
+}
+
