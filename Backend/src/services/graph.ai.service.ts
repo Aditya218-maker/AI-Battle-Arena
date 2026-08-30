0ae59bfb2c22fe3995e7d2601ffea3ceb14378c9
Aditya218-maker <aditya777anand@gmail.com>
2026-08-30 11:31:04 +0530

    feat: define AIBattleState and Judgement types for graph state management

--- Backend/src/services/graph.ai.service.ts
+++ Backend/src/services/graph.ai.service.ts
@@ -1,1 +1,31 @@
-import { StateSchema, MessagesValue, type GraphNode, StateGraph, START, END } from "@langchain/langgraph"
+import { StateSchema, MessagesValue, StateGraph, START, END } from "@langchain/langgraph"
+//every graph has a start and an end node
+
+type Judgement = {
+    winner: "solution_1" | "solution2";
+    solution_1_score: number;
+    solution_2_score: number;
+}
+
+// This AiBAttleState is a custom type.
+// ek graph me ek state ka matlab hota hai ki "jab ek node se data dusre node pe jaarha hota hai toh w o data generally state me jaarha hota hai"
+// ye state hi data ek node se dusre node pe data transfer krti hai
+//ye AIbattleState ek state ka type hai jisme defined hai ki jo state ek node se dusre node jayegi us state me kya kya cheez rahegi
+
+type AIBattleState = {
+    messages: typeof MessagesValue;
+    solution_1: string;
+    solution_2: string;
+    judgement: Judgement;
+}
+
+const state: AIBattleState = {
+    messages: MessagesValue,
+    solution_1: "",
+    solution_2: "",
+    judgement:{
+        winner: "solution_1",
+        solution_1_score: 0,
+        solution_2_score: 0
+    }
+}
